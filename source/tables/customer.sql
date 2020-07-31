78fce3394e7ac4d568e778ff279095bfa1565be0
Christian Ewald <christian.ewald@m-f.ch>
2020-07-31 09:02:42 +0200

    Add customer table

---
+++ source/tables/customer.sql
@@ -0,0 +1,29 @@
+#
+# Copyright (c) Ionplus AG and contributors. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+#
+
+create table customer (
+  number int auto_increment primary key,
+  first_name varchar(40),
+  last_name varchar(60) not null,
+  organisation varchar(255),
+  institute varchar(255),
+  address_1 varchar(80),
+  address_2 varchar(80),
+  town varchar(30),
+  postcode varchar(10),
+  country varchar(30),
+  phone_1 varchar(30),
+  phone_2 varchar(30),
+  fax varchar(30),
+  email varchar(80),
+  www varchar(60),
+  account varchar(40),
+  is_invoice bool default true, # TODO, ewc 2020-07-31: default false? not null?
+  is_correspondence bool default true, # TODO, ewc 2020-07-31: default false? not null?
+  user_comment text,
+  language varchar(2),
+  title varchar(20),
+  salutation varchar(10)
+) engine=innodb;
