a70b757fa97181e019b54c765093e1852c36cfa0
Christian Ewald <christian.ewald@m-f.ch>
2020-07-31 07:29:51 +0200

    Get rid of the test dummy

--- source/brahma.sql
+++ source/brahma.sql
@@ -1,5 +1,4 @@
 
-source dummy.sql;
 source tables/schema_change.sql;
 
 insert into schema_change (version, date_applied) values (1, NOW());
--- source/dummy.sql
+++
@@ -1,5 +0,0 @@
-
-create table dummy (
-    id int auto_increment primary key,
-    title text not null
-) engine=innodb;
