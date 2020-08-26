53e763abe478c74c546e28e8daeab0a2d105818c
Christian Ewald <christian.ewald@m-f.ch>
2020-08-26 15:18:01 +0200

    Run magazine tests using orm

--- test/targets/test_magazine.py
+++ test/targets/test_magazine.py
@@ -4,27 +4,32 @@
 #
 
 import datetime
-import mysql.connector
 import pytest
+from sqlalchemy.exc import IntegrityError
 
 
-def test_timestamp_is_set(db_cursor, db_connection):
-    db_cursor.execute("insert into magazine (name) values ('Test01')")
-    db_connection.commit()
+@pytest.fixture(autouse=True)
+def tear_down(orm):
+    yield
+    orm.rollback()
+    orm.query(orm.magazine).delete()
+    orm.commit()
+
+
+def test_timestamp_is_set(orm):
+    magazine = orm.magazine(name='Test01')
+    orm.add(magazine)
+    orm.commit()
 
     now = datetime.datetime.utcnow()
-
-    db_cursor.execute('select last_changed from magazine')
-    last_changed = db_cursor.fetchall()[0][0]
-
-    delta = abs(last_changed - now)
+    delta = abs(magazine.last_changed - now)
     assert delta.total_seconds() < 5 * 60
 
 
-def test_name_is_unique(db_cursor, db_connection):
-    db_cursor.execute("insert into magazine (name) values ('Test02')")
-    db_connection.commit()
+def test_name_is_unique(orm):
+    orm.add(orm.magazine(name='Test02'))
+    orm.commit()
 
-    with pytest.raises(mysql.connector.IntegrityError):
-        db_cursor.execute("insert into magazine (name) values ('Test02')")
-        db_connection.commit()
+    with pytest.raises(IntegrityError):
+        orm.add(orm.magazine(name='Test02'))
+        orm.commit()
