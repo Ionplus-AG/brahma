60cedaee0f5312d9b1e74065bd15480da652752d
Christian Ewald <christian.ewald@m-f.ch>
2020-09-04 10:49:10 +0200

    Reduce code duplication

--- test/fixtures/database.py
+++ test/fixtures/database.py
@@ -2,12 +2,12 @@
 # Copyright (c) Ionplus AG and contributors. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
-import os
 import pathlib
 
-import mysql.connector
 import pytest
 
+import database
+import database.sql_script
 
 brahma_sql = pathlib.Path(__file__).parent.parent.parent.absolute() / 'schema' / 'brahma.sql'
 legacy_ams_sql = pathlib.Path(__file__).parent / 'legacy_ams.sql'
@@ -24,40 +24,11 @@ def pytest_addoption(parser):
     parser.addini('mysql_database_name', 'The MySQL database name', None, 'brahma_test')
 
 
-def _grep_command(not_matching):
-    if os.name == 'nt':
-        return f'findstr /V /C:"{not_matching}"'
-
-    return f'grep -v "{not_matching}"'
-
-
-def _run_sql_script(script_path, config, database_name):
-    old_cwd = os.getcwd()
-    os.chdir(script_path.parent)
-
-    command = ' '.join([
-        'mysql',
-        '--user', config.getini('mysql_user'),
-        f'-p"{config.getini("mysql_password")}"',
-        '--host', config.getini('mysql_host'),
-        database_name,
-        '<', str(script_path),
-        '2>&1',
-        '|',
-        _grep_command('password on the command line interface can be insecure'),
-        '1>&2',
-    ])
-
-    os.system(command)
-
-    os.chdir(old_cwd)
-
-
 def _create_session(config):
-    return mysql.connector.connect(
+    return database.Session(
         host=config.getini('mysql_host'),
         user=config.getini('mysql_user'),
-        passwd=config.getini('mysql_password'))
+        password=config.getini('mysql_password'))
 
 
 def _prepare_database(config, database_name, script_path):
@@ -67,7 +38,13 @@ def _prepare_database(config, database_name, script_path):
         cursor.execute(f'drop database if exists {database_name}')
         cursor.execute(f'create database {database_name} character set utf8mb4 collate utf8mb4_unicode_ci')
 
-    _run_sql_script(script_path, config, database_name)
+    database.sql_script.run(
+        script_path,
+        config.getini('mysql_host'),
+        config.getini('mysql_user'),
+        config.getini('mysql_password'),
+        database_name,
+    )
 
     session.database = database_name
     session.close()
