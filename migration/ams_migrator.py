79a1c0ff4740d9e16bd84f27c3af10e3e63d30c2
Kishanth Ithayakumar <60095311+kishanth-ithayakumar@users.noreply.github.com>
2021-05-28 08:21:24 +0200

    Task/5928 migrate look up tables (#19)
    * Migrate look up tables
    
    * Sort mapping definitions
    
    * Fix flake8 warnings

--- migration/ams_migrator.py
+++ migration/ams_migrator.py
@@ -12,39 +12,6 @@ class AmsMigrator(Migrator):
         super().__init__(db_session, '_ams_', source_schema, target_schema)
         self.isotope_number = isotope_number
 
-    def migrate_customer(self):
-        return self._map(mappings.customer)
-
-    def migrate_project_advisor(self):
-        return self._execute(self._prepare(queries.migrate_project_advisor))
-
-    def migrate_project(self):
-        return self._map(mappings.project)
-
-    def migrate_sample(self):
-        return self._map(mappings.sample, (mappings.isotope_number(self.isotope_number),))
-
-    def migrate_preparation(self):
-        return self._map(mappings.preparation, (mappings.isotope_number(self.isotope_number),))
-
-    def migrate_target(self):
-        return self._map(mappings.target, (mappings.isotope_number(self.isotope_number),))
-
-    def migrate_magazine(self):
-        return self._execute(self._prepare(queries.migrate_magazine))
-
-    def associate_magazine(self):
-        self._execute(queries.disable_target_triggers)
-        result = self._execute(self._prepare(queries.update_target_set_magazine))
-        self._execute(queries.enable_target_triggers)
-        return result
-
-    def migrate_measurement_sequence(self):
-        self._execute(queries.disable_measurement_sequence_triggers)
-        result = self._execute(self._prepare(queries.migrate_measurement_sequence))
-        self._execute(queries.enable_measurement_sequence_triggers)
-        return result
-
     def migrate_calculation_set(self):
         return self._map(mappings.calculation_set)
 
@@ -53,3 +20,60 @@ class AmsMigrator(Migrator):
 
     def migrate_calculation_sample(self):
         return self._map(mappings.calculation_sample, (mappings.isotope_number(self.isotope_number),))
+
+    def migrate_customer(self):
+        return self._map(mappings.customer)
+
+    def migrate_fraction(self):
+        return self._map(mappings.fraction)
+
+    def migrate_magazine(self):
+        return self._execute(self._prepare(queries.migrate_magazine))
+
+    def migrate_material(self):
+        return self._map(mappings.material)
+
+    def migrate_measurement_sequence(self):
+        self._execute(queries.disable_measurement_sequence_triggers)
+        result = self._execute(self._prepare(queries.migrate_measurement_sequence))
+        self._execute(queries.enable_measurement_sequence_triggers)
+        return result
+
+    def migrate_method(self):
+        return self._map(mappings.method)
+
+    def migrate_preparation(self):
+        return self._map(mappings.preparation, (mappings.isotope_number(self.isotope_number),))
+
+    def migrate_project(self):
+        return self._map(mappings.project)
+
+    def migrate_project_advisor(self):
+        return self._execute(self._prepare(queries.migrate_project_advisor))
+
+    def migrate_project_type(self):
+        return self._map(mappings.project_type)
+
+    def migrate_report_type(self):
+        return self._map(mappings.report_type)
+
+    def migrate_research_type(self):
+        return self._map(mappings.research_type)
+
+    def migrate_sample(self):
+        return self._map(mappings.sample, (mappings.isotope_number(self.isotope_number),))
+
+    def migrate_sample_type(self):
+        return self._map(mappings.sample_type)
+
+    def project_status(self):
+        return self._map(mappings.project_status)
+
+    def migrate_target(self):
+        return self._map(mappings.target, (mappings.isotope_number(self.isotope_number),))
+
+    def associate_magazine(self):
+        self._execute(queries.disable_target_triggers)
+        result = self._execute(self._prepare(queries.update_target_set_magazine))
+        self._execute(queries.enable_target_triggers)
+        return result
--- migration/mappings.py
+++ migration/mappings.py
@@ -30,6 +30,24 @@ customer = TableMapping('user_t', 'customer', (
     ('salutation',),
 ))
 
+fraction = TableMapping('fraction_t', 'fraction', (
+    ('fraction', 'name'),
+    ('indexnr', 'sort_order'),
+))
+
+
+material = TableMapping('material_t', 'material', (
+    ('material', 'name'),
+    ('indexnr', 'sort_order'),
+))
+
+method = TableMapping('method_t', 'preparation_method', (
+    ('method', 'name'),
+    ('descr', 'description'),
+    ('indexnr', 'sort_order'),
+))
+
+
 project = TableMapping('project_t', 'project', (
     ('project_nr', 'number'),
     ('project', 'name'),
@@ -49,6 +67,38 @@ project = TableMapping('project_t', 'project', (
     ('research', 'research_type'),
     ('invoice',),
     ('invoice_date',),
+))
+
+project_status = TableMapping('projectstatus_t', 'project_status', (
+    ('status', 'name'),
+    ('indexnr', 'sort_order'),
+))
+
+project_type = TableMapping('projecttype_t', 'project_type', (
+    ('type', 'name'),
+    ('indexnr', 'sort_order'),
+))
+
+report_type = TableMapping('reporttype_t', 'report_type', (
+    ('type', 'name'),
+    ('indexnr', 'sort_order'),
+))
+
+research_type = TableMapping('research_t', 'research_type', (
+    ('research', 'name'),
+    ('indexnr', 'sort_order'),
+))
+
+sample_type = TableMapping('sampletype_t', 'sample_type', (
+    ('type', 'name'),
+    ('indexnr', 'sort_order'),
+    ('f14c',),
+    ('f14c_sig', 'f14c_sigma'),
+    ('d13c',),
+    ('d13c_sig', 'd13c_sigma'),
+    ('d13c_nom', 'd13c_nominal'),
+    ('blank',),
+    ('active',),
 ))
 
 
